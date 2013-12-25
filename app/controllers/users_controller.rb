21e771fd6137d552948ecc71d93b5e5f2dfa39a7
Audrey Carlsen <audreycarlsen@gmail.com>
2013-12-24 20:29:48 -0800

    First commit

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,34 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to user_path(@user), :notice => 'Welcome to SlothBook, #{@user}!'
+    else
+      render :new
+    end
+  end
+
+  def update
+  end
+
+  def edit
+  end
+
+  def destroy
+  end
+
+  def index
+  end
+
+  def show
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+end
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SlothBook::Application.config.session_store :cookie_store, key: '_sloth_book_session'
