708164a99796d082c06a7360df2792823c6ca200
Audrey Carlsen <audreycarlsen@gmail.com>
2013-12-26 14:17:18 -0800

    Fixed radio button, inverse friendships

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -3,8 +3,7 @@ class FriendshipsController < ApplicationController
   def create
     @friendship = current_user.friendships.build(:friend_id => params[:friend_id])
     if @friendship.save
-      flash[:notice] = 'Friend request sent.'
-      redirect_to root_url
+      redirect_to root_url, notice: "Friend request sent."
     else
       flash[:notice] = 'Unable to add friend.'
       redirect_to root_url
@@ -12,8 +11,9 @@ class FriendshipsController < ApplicationController
   end
 
   def update
-    @friendship = current_user.friendships.find(:id)
+    @friendship = current_user.inverse_friendships.find(params[:id])
     @friendship.status = 'accepted'
+    @friendship.save
     redirect_to root_url
   end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,8 @@ class User < ActiveRecord::Base
   has_many :friends, :through => :friendships
   has_many :inverse_friendships, :class_name => "Friendship", :foreign_key => "friend_id"
   has_many :inverse_friends, :through => :inverse_friendships, :source => :user
+
+  def friendzzz
+    #grabs all friendships and inverse_friendships
+  end
 end
